9b2cb7d578edc2a0ee6b8b913fb206f3e48ee058
Irek Mirgaleev <irek.mirgaleev@acquia.com>
2018-08-26 17:43:57 -0400

    first recording

--- src/app/components/list/list.component.spec.ts
+++ src/app/components/list/list.component.spec.ts
@@ -1,14 +1,23 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ListComponent } from './list.component';
+import { TodoService } from '../../services/todo.service';
+
+class TodoServiceMock {
+  getTodos() {}
+}
 
 describe('ListComponent', () => {
   let component: ListComponent;
   let fixture: ComponentFixture<ListComponent>;
+  let todoServiceMock: TodoService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ListComponent ]
+      declarations: [ ListComponent ],
+      providers: [
+        { provide: TodoService, useClass: TodoServiceMock }
+      ]
     })
     .compileComponents();
   }));
@@ -16,10 +25,17 @@ describe('ListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ListComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+
+    todoServiceMock = TestBed.get(TodoService);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should display a list of todos', () => {
+
+    spyOn(todoServiceMock, 'getTodos').and.returnValue(['one', 'two', 'three']);
+
+    fixture.detectChanges();
+
+    expect(fixture.nativeElement.querySelectorAll('.todo').length).toBe(3);
+
   });
 });
--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoService } from '../../services/todo.service';
 
 @Component({
   selector: 'app-list',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  todos = [];
+
+  constructor(private todoService: TodoService) { }
 
   ngOnInit() {
+    this.todos = this.todoService.getTodos();
   }
 
 }

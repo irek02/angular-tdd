a6528b55b198405e1658c9275489f74a388adafe
Irek Mirgaleev <irek.mirgaleev@acquia.com>
2018-09-30 10:38:05 -0400

    preparing for the second test - per stock info

--- src/app/components/stock-list/stock-list.component.spec.ts
+++ src/app/components/stock-list/stock-list.component.spec.ts
@@ -14,14 +14,37 @@ fdescribe('StockListComponent', () => {
     };
   };
 
+  const mockedStocks = [
+    {
+      id: 1,
+      name: 'Apple',
+      sharePrice: 195
+    },
+    {
+      id: 2,
+      name: 'Tesla',
+      sharePrice: 115
+    },
+    {
+      id: 3,
+      name: 'CocaCola',
+      sharePrice: 98
+    },
+    {
+      id: 4,
+      name: 'GE',
+      sharePrice: 90
+    }
+  ];
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ StockListComponent ],
+      declarations: [StockListComponent],
       providers: [
         StockService
       ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -32,12 +55,22 @@ fdescribe('StockListComponent', () => {
 
   it('should list stocks', () => {
 
-    spyOn(stockService, 'getStocks').and.returnValue(['Apple', 'Tesla', 'CocaCola', 'GE']);
+    spyOn(stockService, 'getStocks').and.returnValue(mockedStocks);
 
     fixture.detectChanges();
 
     expect(elements().stocks.length).toBe(4);
 
   });
+
+  // it('should show info for each stock', () => {
+
+  //   spyOn(stockService, 'getStocks').and.returnValue(['Apple', 'Tesla', 'CocaCola', 'GE']);
+
+  //   fixture.detectChanges();
+
+  //   expect(elements().stocks.length).toBe(4);
+
+  // });
 
 });
